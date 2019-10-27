ccf122b0fe15bf8a2243b1d2e01e03405f87bd82
brasspots <44511851+brasspots@users.noreply.github.com>
2019-10-27 13:19:00 +0000

    Checkpoint callbacks.c
    added callbacks to change screen
    checkpoint for fixing how widgets are passed to the callbacks

--- callbacks.c
+++ callbacks.c
@@ -20,3 +20,41 @@ void destroy(GtkWidget window, gpointer data) {
 	gtk_main_quit();
 
 }
+
+// show answer callback
+void show_answer(GtkWidget question_event_box, GtkWidget ** main_screens_array) {
+
+	// hide all the widgets while the switch takes place
+	gtk_widget_hide(main_screens_array[MAIN_SCREEN]);
+
+	// remove the question vbox from the main screen vbox
+	gtk_container_remove(GTK_CONTAINER (main_screens_array[MAIN_SCREEN]), main_screens_array[QUESTION_SCREEN]);
+
+	// pack in the answer screen
+	gtk_box_pack_start(GTK_BOX (main_screens_array[MAIN_SCREEN]), main_screens_array[ANSWER_SCREEN], TRUE, TRUE, 5);
+
+	// show the new widgets
+	gtk_widget_show_all(main_screens_array[MAIN_SCREEN]);
+
+};
+
+// next question callback
+void next_question(GtkWidget button, GtkWidget ** main_screens_array) {
+
+	// record stats based on which button was pressed
+
+	// hide all the widgets while the switch takes place
+	gtk_widget_hide(main_screens_array[MAIN_SCREEN]);
+
+	// get new question and insert its information into quetsion and answer boxes
+
+	// remove the answer vbox from main screen vbox
+	gtk_container_remove(GTK_CONTAINER (main_screens_array[MAIN_SCREEN]), main_screens_array[ANSWER_SCREEN]);
+
+	// pack in the question screen
+	gtk_box_pack_start(GTK_BOX (main_screens_array[MAIN_SCREEN]), main_screens_array[QUESTION_SCREEN], TRUE, TRUE, 5);
+
+	// show the widgets
+	gtk_widget_show_all(main_screens_array[MAIN_SCREEN]);
+
+};
