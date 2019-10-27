8e6f4836c59797f4841acee2a1476f3900c26dd8
brasspots <44511851+brasspots@users.noreply.github.com>
2019-10-27 13:20:32 +0000

    Checkpoint callbacks.h
    added definitions for accessing widget array
    added prototypes for changing screen callbacks

--- callbacks.h
+++ callbacks.h
@@ -14,8 +14,20 @@
 #ifndef FLASH_CARD_CALLBACKS
 #define FLASH_CARD_CALLBACKS
 
+// define array indexes for the main screens array
+#define MAIN_SCREEN 0
+#define QUESTION_SCREEN 1
+#define ANSWER_SCREEN 2
+#define ADD_SCREEN 3
+
 // destroy window callback
 void destroy(GtkWidget window, gpointer data);
+
+// change main screen to answer vbox
+void show_answer(GtkWidget question_event_box, GtkWidget ** main_screens_array);
+
+// change main screen to question box
+void next_question(GtkWidget button, GtkWidget ** main_screens_array);
 
 // end the guard
 #endif
