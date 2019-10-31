ebd8135f1e7dde6ff1d498af51599bb9a1fa28ff
brasspots <44511851+brasspots@users.noreply.github.com>
2019-10-31 15:28:00 +0000

    Update callbacks.h
    screen switching works with 0 global variables, yay!

--- callbacks.h
+++ callbacks.h
@@ -14,20 +14,25 @@
 #ifndef FLASH_CARD_CALLBACKS
 #define FLASH_CARD_CALLBACKS
 
-// declare global variables
-extern GtkWidget * main_screen_vbox;
+// typedef struct to pass main_screen_vbox and another  widget to callback functions
+typedef struct {
+
+	GtkWidget * main_screen_vbox;
+	GtkWidget * new_child;
+
+} change_screen_data;
 
 // destroy window callback
 void destroy(GtkWidget window, gpointer data);
 
 // change main screen to question vbox with a correct answer
-void next_question_correct(GtkWidget button, GtkWidget * question_vbox);
+void next_question_correct(GtkWidget button, change_screen_data * data);
 
 // change the main screen to question vbox with an incorrect answer
-void next_question_incorrect(GtkWidget button, GtkWidget * question_vbox);
+void next_question_incorrect(GtkWidget button, change_screen_data * data);
 
 // change the main screen
-void change_screen(GtkWidget widget, GtkWidget * new_child);
+void change_screen(GtkWidget widget, change_screen_data * data);
 
 // end the guard
 #endif
