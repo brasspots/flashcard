fc31618dc5e45c7dee9568a14e99a4db178b4317
brasspots <44511851+brasspots@users.noreply.github.com>
2019-10-31 15:27:21 +0000

    Update callbacks.c
    screen switching works with 0 global variables, yay!

--- callbacks.c
+++ callbacks.c
@@ -22,40 +22,40 @@ void destroy(GtkWidget window, gpointer data) {
 };
 
 // next question callback
-void next_question_correct(GtkWidget button, GtkWidget * question_vbox) {
+void next_question_correct(GtkWidget button, change_screen_data * data) {
 
 	// do correct stuff
 
 	// change the screen
-	change_screen(button, question_vbox);
+	change_screen(button, data);
 
 };
 
-void next_question_incorrect(GtkWidget button, GtkWidget * question_vbox) {
+void next_question_incorrect(GtkWidget button, change_screen_data * data) {
 
 	// do incorrect stuff
 
 	// change the screen
-	change_screen(button, question_vbox);
+	change_screen(button, data);
 
 };
 
-void change_screen(GtkWidget widget, GtkWidget * new_child) {
+void change_screen(GtkWidget widget, change_screen_data * data) {
 
 
 	// acquire current child
-	GtkWidget * current_child = gtk_container_get_children(GTK_CONTAINER (main_screen_vbox))->data;
+	GtkWidget * current_child = gtk_container_get_children(GTK_CONTAINER (data->main_screen_vbox))->data;
 
 	// make a reference to the current child so it isn't hit by garbage collection when we remove it
 	g_object_ref(current_child);
 
 	// remove the current child from the main screen vbox
-	gtk_container_remove(GTK_CONTAINER (main_screen_vbox), current_child);
+	gtk_container_remove(GTK_CONTAINER (data->main_screen_vbox), current_child);
 
 	// add the new child
-	gtk_box_pack_start(GTK_BOX (main_screen_vbox), new_child, TRUE, TRUE, 5);
+	gtk_box_pack_start(GTK_BOX (data->main_screen_vbox), data->new_child, TRUE, TRUE, 5);
 
 	// show the new child
-	gtk_widget_show_all(main_screen_vbox);
+	gtk_widget_show_all(data->main_screen_vbox);
 
 };
