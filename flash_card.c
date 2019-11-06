2c5847ed9697bd9068dfbff734099d3ac7fd8603
brasspots <44511851+brasspots@users.noreply.github.com>
2019-11-06 08:56:24 +0000

    Checkpoint flashcard.c
    Added a button to show answer from the question screen
    Added mnemonics to all buttons
    Moved correct/incorrect buttons to 'stick' to the walls of the window

--- flash_card.c
+++ flash_card.c
@@ -21,13 +21,13 @@
 int main(int argc, char ** argv){
 
 	// define gtk container widgets for construction
-	GtkWidget * window, * total_vbox, * header_buttons_hbox, * question_event_box, * answer_buttons_hbox;
+	GtkWidget * window, * total_vbox, * header_buttons_hbox, * question_event_box, * answer_buttons_hbox, * question_buttons_hbox;
 
 	// define main gtk container widgets
 	GtkWidget * question_vbox, * answer_vbox, * add_question_vbox, * main_screen_vbox;
 
 	// define gtk button and label widgets
-	GtkWidget * menu_button, * correct_button, * incorrect_button, * filters_button, * tags_label, * question_label, * answer_label;
+	GtkWidget * menu_button, * correct_button, * incorrect_button, * filters_button, * tags_label, * question_label, * answer_label, * show_answer_button;
 
 	// initialise gtk
 	gtk_init(&argc, &argv);
@@ -48,6 +48,7 @@ int main(int argc, char ** argv){
 	header_buttons_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	question_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 	question_event_box = gtk_event_box_new();
+	question_buttons_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	answer_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
 	answer_buttons_hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 	add_question_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
@@ -58,10 +59,11 @@ int main(int argc, char ** argv){
 	answer_label = gtk_label_new("#ANSWER");
 
 	// assign new buttons to all who need buttons
-	menu_button = gtk_button_new_with_label("#MENU");
-	filters_button = gtk_button_new_with_label("#FILTERS");
-	correct_button = gtk_button_new_with_label("#CORRECT");
-	incorrect_button = gtk_button_new_with_label("#INCORRECT");
+	menu_button = gtk_button_new_with_mnemonic("#_MENU");
+	filters_button = gtk_button_new_with_mnemonic("#_FILTERS");
+	correct_button = gtk_button_new_with_mnemonic("#_CORRECT");
+	incorrect_button = gtk_button_new_with_mnemonic("#_INCORRECT");
+	show_answer_button = gtk_button_new_with_mnemonic("#_SHOW__ANSWER");
 
 	// contruct the header buttons to go in total box to go in window
 	gtk_box_pack_start(GTK_BOX (header_buttons_hbox), menu_button, TRUE, TRUE, 5);
@@ -79,13 +81,17 @@ int main(int argc, char ** argv){
 	// add the question label to the question event box
 	gtk_container_add(GTK_CONTAINER (question_event_box), question_label);
 
+	// add the show answer button to the question buttons hbox
+	gtk_box_pack_start(GTK_BOX (question_buttons_hbox), show_answer_button, FALSE, FALSE, 5); // do not grow this button to fill space
+
 	// contruct the question vbox to go in main screen vbox
+	gtk_box_pack_start(GTK_BOX (question_vbox), tags_label, FALSE, FALSE, 5); // do not grow this label to fill spare space
 	gtk_box_pack_start(GTK_BOX (question_vbox), question_event_box, TRUE, TRUE, 5);
-	gtk_box_pack_start(GTK_BOX (question_vbox), tags_label, FALSE, FALSE, 5); // dom not grow this label to fill spare space
+	gtk_box_pack_start(GTK_BOX (question_vbox), question_buttons_hbox, FALSE, FALSE, 5); // do not grow this container to fill space
 
 	// construct the answer buttons hbox to go in answer vbox (do not grow them out into spare space)
-	gtk_box_pack_end(GTK_BOX (answer_buttons_hbox), correct_button, TRUE, FALSE, 5);
-	gtk_box_pack_end(GTK_BOX (answer_buttons_hbox), incorrect_button, TRUE, FALSE, 5); 
+	gtk_box_pack_start(GTK_BOX (answer_buttons_hbox), correct_button, FALSE, FALSE, 5);
+	gtk_box_pack_end(GTK_BOX (answer_buttons_hbox), incorrect_button, FALSE, FALSE, 5); 
 
 	// construct the answer vbox to go in main screen vbox
 	gtk_box_pack_start(GTK_BOX (answer_vbox), answer_label, TRUE, TRUE, 5);
@@ -104,8 +110,9 @@ int main(int argc, char ** argv){
 	// following callbacks are connected swapped for reasons unknown
 	// TODO: why?
 
-	// connect the question event box clicked callback to show answer
+	// connect the question event box clicked and show answer button clicked callbacks to show answer
 	g_signal_connect_swapped(G_OBJECT (question_event_box), "button_press_event", G_CALLBACK (change_screen), &to_answer);
+	g_signal_connect_swapped(G_OBJECT (show_answer_button), "clicked", G_CALLBACK (change_screen), &to_answer);
 
 	// connect the correct button clicked callback to next question
 	g_signal_connect_swapped(G_OBJECT (correct_button), "clicked", G_CALLBACK (next_question_correct), &to_question);
